f68abb57293c2f158c87c31c012a6fe892b66b55
Hirofumi Seiyama <hseiyama@gmail.com>
2022-08-02 22:19:42 +0900

    lookupTransform の高度なAPIを指定

--- src/learning_tf2_cpp/src/turtle_tf2_listener.cpp
+++ src/learning_tf2_cpp/src/turtle_tf2_listener.cpp
@@ -74,10 +74,13 @@ private:
         // and send velocity commands for turtle2 to reach target_frame
         try {
           rclcpp::Time now = this->get_clock()->now();
+          rclcpp::Time when = now - rclcpp::Duration(5, 0);
           transformStamped = tf_buffer_->lookupTransform(
             toFrameRel,
-            fromFrameRel,
             now,
+            fromFrameRel,
+            when,
+            "world",
             50ms);
         } catch (tf2::TransformException & ex) {
           RCLCPP_INFO(
